e3870038ff53a42e15d86f2079568b1045400f97
NehaSutar27 <nehalaturkar@gmail.com>
2025-01-29 20:40:24 +0000

    Tailwind Css NPM Installation

---
+++ NPMTailwind/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./index.html",
+      "./src/**/*.{html,js}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+
+    theme:{
+        colors: {
+           transparent: 'transparent',
+           white: '#ffffff',
+           black: '#000000',
+           tahiti: '#3ab7bf',
+      },
+      extend: {},
+      },
+  }
+  
